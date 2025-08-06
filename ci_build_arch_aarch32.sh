597418810e6ff7580e96976ab7426a8a0a89fbb6
Maksim Belov <ultramaksim@gmail.com>
2025-08-06 19:38:41 +0300

    Fix[ci_build_aarch32]: specify correct target triplet

--- ci_build_arch_aarch32.sh
+++ ci_build_arch_aarch32.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-export TARGET=arm-linux-androideabi21
+export TARGET=armv7a-linux-androideabi21
 export TARGET_JDK=arm
 export NDK_PREBUILT_ARCH=/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/arm-linux-androideabi/bin/strip
 
