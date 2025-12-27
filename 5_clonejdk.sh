4caa863f350e8253b26d5e5401ef2bb6792db3e1
Maksim Belov <ultramaksim@gmail.com>
2025-12-27 22:40:10 +0300

    Feat[jdk]: initial changes for jdk 25

--- 5_clonejdk.sh
+++ 5_clonejdk.sh
@@ -3,6 +3,8 @@ set -e
 
 if [[ $TARGET_VERSION -eq 21 ]]; then
     git clone --branch jdk21.0.1 --depth 1 https://github.com/openjdk/jdk21u openjdk-21
+elif [[ $TARGET_VERSION -eq 25 ]]; then
+    git clone --depth 1 https://github.com/openjdk/jdk25u openjdk-25
 else
     git clone --depth 1 https://github.com/openjdk/jdk17u openjdk-17
 fi
