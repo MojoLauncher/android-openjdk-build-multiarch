376bbef99509c4b4639b0b78173406f5ce56933e
Maksim Belov <ultramaksim@gmail.com>
2026-03-29 00:59:53 +0300

    Enable aarch32 and x86_64

--- maketoolchain.sh
+++ maketoolchain.sh
@@ -6,9 +6,9 @@ set -e
 mkdir -p $NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain
 pushd $NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain
 
-wget https://github.com/MojoLauncher/gcc-toolchain/releases/download/prebuilt/gcc-13-arm64-21.tar.xz
-tar xf gcc-13-arm64-21.tar.xz
-rm gcc-13-arm64-21.tar.xz
+wget https://github.com/MojoLauncher/gcc-toolchain/releases/download/prebuilt/gcc-13-${TARGET_SHORT}-21.tar.xz
+tar xf gcc-13-${TARGET_SHORT}-21.tar.xz
+rm gcc-13-${TARGET_SHORT}-21.tar.xz
 
 popd
 
