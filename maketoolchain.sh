ab1276257d905bee30b4748b56c2160b21b4b8fe
Maksim Belov <ultramaksim@gmail.com>
2026-03-28 19:42:42 +0300

    Disable other arches

--- maketoolchain.sh
+++ maketoolchain.sh
@@ -3,8 +3,6 @@ set -e
 
 . setdevkitpath.sh
 
-if [[ "TARGET_JDK" == "aarch64" ]]; then
-
 mkdir $NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain
 pushd $NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain
 
@@ -14,12 +12,4 @@ rm gcc-13-arm64-21.tar.xz
 
 popd
 
-else
-  
-$NDK/build/tools/make-standalone-toolchain.sh \
-	--arch=${TARGET_SHORT} \
-	--platform=android-21 \
-	--install-dir=$NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain
-
-fi
 cp devkit.info.${TARGET_SHORT} $NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain/
