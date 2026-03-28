687f9621241241173ed972caf4cd507337f58c99
Maksim Belov <45949002+artdeell@users.noreply.github.com>
2026-03-28 19:37:21 +0300

    Experiment: use gcc 13 ARM64 toolchain

--- maketoolchain.sh
+++ maketoolchain.sh
@@ -2,9 +2,24 @@
 set -e
 
 . setdevkitpath.sh
+
+if [[ "TARGET_JDK" == "aarch64" ]]; then
+
+mkdir $NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain
+pushd $NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain
+
+wget https://github.com/MojoLauncher/gcc-toolchain/releases/download/prebuilt/gcc-13-arm64-21.tar.xz
+tar xf gcc-13-arm64-21.tar.xz
+rm gcc-13-arm64-21.tar.xz
+
+popd
+
+else
   
 $NDK/build/tools/make-standalone-toolchain.sh \
 	--arch=${TARGET_SHORT} \
 	--platform=android-21 \
 	--install-dir=$NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain
+
+fi
 cp devkit.info.${TARGET_SHORT} $NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain/
