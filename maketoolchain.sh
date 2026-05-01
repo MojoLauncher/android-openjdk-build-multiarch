6aa67775328a481075d467f598f124ec0af219ce
Maksim Belov <45949002+artdeell@users.noreply.github.com>
2026-05-01 08:28:07 +0300

    Fix[maketoolchain]: use correct toolchain file name

--- maketoolchain.sh
+++ maketoolchain.sh
@@ -7,21 +7,21 @@ mkdir -p $NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain
 pushd $NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain
 
 if [[ "$TARGET_SHORT" == "arm" ]]; then
-   export TARGET_API=9
+   export TOOLCHAIN_TAR=gcc-13-${TARGET_SHORT}-9.tar.xz
 else
-   export TARGET_API=21
+   export TOOLCHAIN_TAR=gcc-13-${TARGET_SHORT}-21.tar.xz
 fi
 
-wget -nc -nv https://github.com/MojoLauncher/gcc-toolchain/releases/download/prebuilt/gcc-13-${TARGET_SHORT}-${TARGET_API}.tar.xz
-tar xf gcc-13-${TARGET_SHORT}-21.tar.xz
-rm gcc-13-${TARGET_SHORT}-21.tar.xz
+wget -nc -nv https://github.com/MojoLauncher/gcc-toolchain/releases/download/prebuilt/$TOOLCHAIN_TAR
+tar xf $TOOLCHAIN_TAR
+rm $TOOLCHAIN_TAR
 
-#set +e
+set +e
 # I didn't pay enough attention :(
 # Remove old libstdc++.so for gcc 4.9, to force the compiler into using the fresher one in aarch64-linux-android/lib64/
 rm sysroot/usr/lib/libstdc++.a
 rm sysroot/usr/lib/libstdc++.so
-#set -e
+set -e
 
 popd
 
